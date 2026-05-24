22959f4e8576f9cb593f7f1fc5f2bcfe02270113
Faria Jaheen <171293812+FariaJaheen@users.noreply.github.com>
2026-05-24 17:10:01 -0400

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        ss = sorted(s)
+        tt = sorted(t)
+        for i in range(len(ss)):
+            if ss[i] == tt[i]:
+                return True
+        return False
+        
