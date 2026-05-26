46009611601ded4c1151aa4fcf1df701addc78fe
Faria Jaheen <171293812+FariaJaheen@users.noreply.github.com>
2026-05-26 15:19:13 -0400

    Add: python-multiple-parameters - submission-1

---
+++ Python For Beginners/python-multiple-parameters/submission-1.py
@@ -0,0 +1,19 @@
+def two_sum(a, b):
+    sum_2 = a+b 
+    print(sum_2) 
+
+def three_sum(c, d, e):
+    sum_3 = c + d + e
+    print(sum_3)
+
+
+# do not modify below this line
+two_sum(7,10)
+three_sum(3, 5, 8)
+
+
+
+
+# do not modify below this line
+two_sum(10, 9)
+three_sum(5, 14, 6)
